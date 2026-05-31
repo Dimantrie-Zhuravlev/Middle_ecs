208c13e3a6439a0334fe74189a9f6268d21ceeb1
Dmitriy Zhuravlev <tankerlord@mail.ru>
2026-05-31 20:24:10 +0300

    Компоненты здоровья и ловушка с уроном

---
+++ Assets/!Project/Scripts/PlayerStats.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace.Components.Interfaces
+{
+    [Serializable]
+    public class PlayerStats 
+    {
+        public int shotsCount;
+    }
+}
